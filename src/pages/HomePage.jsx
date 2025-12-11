67954e2c222321ed476924b030c245295577d504
David Bouhaben <davidbouhaben@yahoo.co.id>
2025-12-11 21:11:46 +0700

    fix: update HomePage and StudentDetailsPage for improved routing with NavLink

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import StudentCard from "../components/StudentCard";
 
 import studentsData from "../assets/students.json";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 function HomePage() {
   // eslint-disable-next-line no-unused-vars
@@ -24,15 +24,15 @@ function HomePage() {
         <span style={{ flexBasis: "20%" }}>Program</span>
         <span style={{ flexBasis: "20%" }}>Email</span>
         <span style={{ flexBasis: "20%" }}>Phone</span>
-      </div>
-
-      <Link to="/students/:studentId">
-        {" "}
-        {students &&
-          students.map((student) => {
-            return <StudentCard key={student._id} {...student} />;
-          })}
-      </Link>
+      </div>{" "}
+      {students &&
+        students.map((student) => {
+          return (
+            <NavLink key={student._id} to={`/students/${student._id}`}>
+              <StudentCard key={student._id} {...student} />;
+            </NavLink>
+          );
+        })}
     </div>
   );
 }
